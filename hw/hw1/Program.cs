1c0b579c774d7720406b8a876a3717d5a0c6dc09
prcstak <g.marsel2002@gmail.com>
2021-09-15 21:37:24 +0300

    first commit

---
+++ hw/hw1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
