5845a859e1abc49c371856af986835706dccd12b
prcstak <g.marsel2002@gmail.com>
2021-09-17 09:37:51 +0300

    Added all basic operations

--- hw/hw1/Program.cs
+++ hw/hw1/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace hw1
+namespace Homework1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            if (!int.TryParse(args[0], out int arg1))
+            {
+                Console.WriteLine(args[0] + args[1] + args[2] + " Incorrect arguments ");
+                return 1;
+            }
+            if (!int.TryParse(args[2], out int arg2))
+            {
+                Console.WriteLine(args[0] + args[1] + args[2] + " Incorrect arguments ");
+                return 1;
+            }
+            var availableOperation = new List<string> { "+", "-", "*", "/", };
+            if (!availableOperation.Contains(args[1]))
+            {
+                Console.WriteLine(args[0] + args[1] + args[2] + " Incorrect operation ");
+                return 2;
+            }
+
+
+            var result = args[1] switch
+            {
+                "+" => arg1 + arg2,
+                "-" => arg1 - arg2,
+                "*" => arg1 * arg2,
+                "/" => arg1 / arg2,
+                _ => 0,
+            };
+
+
+            Console.Write(args[0] + args[1] + args[2] + $"={result}");
+            return 0;
         }
     }
 }
