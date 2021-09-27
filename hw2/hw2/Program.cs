319c4c6f3072b5c54067dc1af27212364a97885a
prcstak <g.marsel2002@gmail.com>
2021-09-27 17:39:42 +0300

    create .ilproj

---
+++ hw2/hw2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(IL.Class.Square(8, 2));
+        }
+    }
+}
